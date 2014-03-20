9e72bd6917d006e5346ea3b3c2e42127f85e5aa1
theflyfish <yucareer@163.com>
2014-03-20 10:42:22 +0800

    update for the input param parse

--- main.c
+++ main.c
@@ -18,102 +18,38 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdarg.h>
+#include <pthread.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <net/if.h>
 #include <sys/types.h>
 #include <signal.h>
 #include <getopt.h>
-#include <pthread.h>
+
+//#include <unistd.h>
 //#include "utils.h"
 #include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
+#include <semaphore.h>
 #include "libav-compat.h"
 #include "istb_ringbuffer.h"
 #include "istb_httpserver.h"
 #include "istb_m3u8.h"
+#include "istb_mongoose.h"
+#include "istb_udp.h"
 
+
+static sem_t* g_HttpServer_Sem = NULL;
+int myterminate=0;
+static RingBuffer  *buffer_queue=NULL;
 
 #define BF_RINGBUF_SIZE (188*1024*10*10*5)
-#define FILE_SERVER_BUFF_SIZE (188*10)
-#define VLC_Test_File_Path  "/home/yuxiang/workspace/m3u8-test/src/test.ts"
-static int isNeedShutDown=0;
-static RingBuffer * buffer_queue=NULL;
-void Buffer_FileServer_Task(void)
-{
-    char ReadBuf_file[FILE_SERVER_BUFF_SIZE]={0};
-    FILE * fp_VlCTest=NULL;
-    int have_read=0;
-    long int file_size=0;
-    long int file_pos=0;
-    //int have_send=0;
-    printf("coming..\n");
-    fp_VlCTest = fopen(VLC_Test_File_Path, "rb+" );
-    if(fp_VlCTest == NULL)
-    {
-        printf("can not open file!\n");
-        return ;
-    }
-    fseek(fp_VlCTest,0,SEEK_END); 
-    file_size = ftell(fp_VlCTest);
-    //fseek(fp_VlCTest,0,SEEK_SET); 
-    rewind(fp_VlCTest);
-    printf(":xxxfile_size:%ld,fp:%p \n",file_size,fp_VlCTest);
-    do{ 
-        if (isNeedShutDown)
-        {
-            printf("stop file server \n");
-            break;
-        }
-        have_read = fread(ReadBuf_file, sizeof(char), FILE_SERVER_BUFF_SIZE, fp_VlCTest); 
-        if (have_read!=FILE_SERVER_BUFF_SIZE)
-        {
-            if ((file_pos+have_read)==file_size)
-            {
-                printf("read file get end!\n"); 
-            }
-            else
-            {
-                printf("read file error!\n"); 
-                break;
-            }
-        }
-        file_pos += have_read;
-        //printf(" send live streamer \n");
-        iSTB_Buffer_write(buffer_queue,ReadBuf_file,FILE_SERVER_BUFF_SIZE);
-        //printf("%s:have send=%d \n",__FUNCTION__,have_send);
-        //usleep(100);//0.1ms
-        usleep(1000*10);//10ms
-
-    }while(file_pos < file_size);
-    fclose(fp_VlCTest);
-    printf("file streamer push finished !\n");
-    usleep(1000*1000);//1s
-    return; 
-}
-
-int Buffer_FileServer_Start(void)
-{
-    int s=0;
-    pthread_attr_t attr;
-    pthread_t      ThreadId;
-   // int ret =0;
 
 
-
-    s = pthread_attr_init(&attr);
-    pthread_attr_setdetachstate(&attr,PTHREAD_CREATE_DETACHED);
-    
-    if(pthread_create (&ThreadId, &attr, (void *(*)(void *))Buffer_FileServer_Task, NULL))
-    {
-        printf("create http server thread error \n");
-        pthread_attr_destroy(&attr);
-        return -1;
-    }
-    s = pthread_attr_destroy(&attr);
-    return 0;
-}
-
-int myterminate=0;
-FILE* file=NULL;
-int read_data(void *opaque, uint8_t *buf, int buf_size) {
+int m3u8_read_data(void *opaque, uint8_t *buf, int buf_size) {
 //	UdpParam udphead;
 	int size = buf_size;
 	int ret;
@@ -130,36 +66,212 @@ int read_data(void *opaque, uint8_t *buf, int buf_size) {
 	//printf("read data Ok %d\n", filled);
 	return filled;
 }
-pthread_t httpserver_tid=0;
-int  M3u8_Event_Callback(int eventtype)
-{
-    printf("laalal %d \n",eventtype);
-    if (eventtype == M3U8_SEGMENT_READY) {
-        httpserver_tid=istb_httpserver_create(8080,".");
-    }
-    return 0; 
-}
 
-
-int  main(void)
-{
-    pthread_t m3u8_tid=0;
-    #if 1
-    #endif
-    #if 1
-    buffer_queue=iSTB_Buffer_create(BF_RINGBUF_SIZE); 
-    Buffer_FileServer_Start();
-    istb_m3u8_event_subscribe(M3u8_Event_Callback);
-    m3u8_tid=istb_m3u8_start("-n 2 -d 10 -p tmp/istb -m tmp/yuxiang.m3u8 -u http://127.0.0.1:8080/",read_data);
-    printf("%s:sleep 30s \n",__FUNCTION__);
-    usleep(1000*1000*30);//5 min
-    printf("%s:now destory \n",__FUNCTION__);
-    myterminate=1;
-    istb_m3u8_stop(m3u8_tid);
-    istb_httpserver_destroy(httpserver_tid);
-    iSTB_Buffer_destroy(buffer_queue);
-    #endif
+int udp_write_data(char  *buf, int len){
+    //printf("%s:coming.... \n",__FUNCTION__);
+    iSTB_Buffer_write(buffer_queue,buf,len);
     return 0;
 }
 
+
+
+int  M3u8_Event_Callback(int eventtype)
+{
+    printf("%s:m3u8 %d \n",__FUNCTION__,eventtype);
+    if (eventtype == M3U8_SEGMENT_READY) {
+        sem_post(g_HttpServer_Sem);
+    }
+    return 0; 
+}
+int  Udp_Event_Callback(int eventtype)
+{
+    printf("%s:udp %d \n",__FUNCTION__,eventtype);
+    return 0; 
+}
+
+void main_handler(int signum) {
+    (void)signum;
+    myterminate = 1;
+}
+/*
+-i:    input stream address  as: 127.0.0.1:1234 , used by udp modules
+-o:  the output  address  for   hls segment streams,  used by  httpserver  modules, as: 192.168.1.15:8080
+-n:  the maxnum of stream  segments  created by  m3u8 modules 
+-d:  by second,the duration of every stream segment,used by m3u8 modules 
+-p:  the prefix for segment name,such as: walk-1.ts\walk-2.ts\walk-3.ts, for the m3u8 index file 
+-m: the file name of m3u8 file 
+*/  
+// ./istb-m3u8 -i 127.0.0.1:1234     -o  8909  -n 15 -d 10 -p istb -m yuxiang.m3u8  -u  http://127.0.0.1:8080/  
+//  ./istb-m3u8 -i 127.0.0.1:1234     -o  8909  -n 15 -d 10 -p istb -m yuxiang.m3u8  
+// ./istb-m3u8 -i 127.0.0.1:1234     -o  127.0.0.1:8909  -n 15 -d 10 -p istb -m yuxiang.m3u8  
+//./iUDPtoHLS -i 127.0.0.1:1234     -o  127.0.0.1:8909  -n 5 -d 10 -p istb -m yuxiang.m3u8  
+typedef struct{
+    char * input_ip;
+    int input_port;
+    char *output_ip;
+    int output_port;
+    int m3u8_maxnum;
+    int m3u8_duration;
+    char *m3u8_prefix;
+    char *m3u8_filename;
+    char *m3u8_uri_prefix;
+}io_options_t;
+
+int  main(int argc,char *argv[])
+{
+    pthread_t httpserver_tid=0;
+    pthread_t m3u8_tid=0;
+    pthread_t udp_tid=0;
+    struct sigaction act;
+    int ch;
+    char *p=NULL;
+    io_options_t opts;
+    char buf[128]={0};
+   #if 1
+    while ((ch = getopt(argc, argv, "i:o:n:d:p:m:h")) != -1)
+    {
+        switch (ch) {
+        case 'i':
+            printf("%c: %s\n", ch,optarg);
+            /* strtok places a NULL terminator 
+            in front of the token, if found */ 
+            p = strtok(optarg, ":"); 
+            if (p) {
+                 printf("%s \n", p); 
+                 opts.input_ip=strdup(p);
+            }
+            /* A second call to strtok using a NULL 
+            as the first parameter returns a pointer 
+            to the character following the token  */ 
+            p = strtok(NULL, ":"); 
+            if (p){
+                printf("%s \n", p);
+                opts.input_port=strtol(p,NULL,10);
+            }
+            break;
+        case 'o':
+            printf("%c :%s\n", ch,optarg);
+            p = strtok(optarg, ":"); 
+            if (p) {
+                 printf("%s \n", p); 
+                 opts.output_ip=strdup(p);
+            }
+            p = strtok(NULL, ":"); 
+            if (p){
+                printf("%s \n", p);
+                opts.output_port=strtol(p,NULL,10);
+            }
+            break;
+        case 'n':
+            printf("%c :%s\n",ch, optarg);
+            opts.m3u8_maxnum=strtol(optarg,NULL,10);
+            break;
+        case 'd':
+            printf("%c: %s\n",ch, optarg);
+            opts.m3u8_duration=strtol(optarg,NULL,10);
+            break;
+        case 'p':
+            printf("%c :%s\n",ch, optarg);
+            opts.m3u8_prefix=strdup(optarg);
+            break;
+        case 'm':
+            printf("%c :%s\n",ch, optarg);
+            opts.m3u8_filename=strdup(optarg);
+            break;
+        case 'h':
+            printf("-h: yeah! you get the  secret! \n"
+                    "-i:  input stream address  as: 127.0.0.1:1234 , used by udp modules \n"
+                    "-o:  the output  address  for   hls segment streams,  used by  httpserver  modules, as: 192.168.1.15:8080 \n"
+                    "-n:  the maxnum of stream  segments  created by  m3u8 modules \n"
+                    "-d:  the duration of every stream segment,used by m3u8 modules \n" 
+                    "-p:  the prefix for segment name,such as: walk-1.ts\walk-2.ts\walk-3.ts, for the m3u8 index file \n"
+                   "-m:  the file name of m3u8 file \n");
+            break;
+        default:
+            printf("unknown tag %c \n",ch);
+            break;
+        }
+    }
+
+    if (!opts.input_ip) {
+        printf("%d:param error \n",__LINE__);
+        return 1;
+    }
+    if (!opts.output_ip) {
+        printf("%d:param error \n",__LINE__);
+        return 1;
+    }
+    if (!opts.m3u8_filename) {
+        printf("%d:param error \n",__LINE__);
+        return 1;
+    }
+    if (!opts.m3u8_prefix) {
+        printf("%d:param error \n",__LINE__);
+        return 1;
+    }
+
+    snprintf(buf,sizeof(buf),"http://%s:%d/",opts.output_ip,opts.output_port);
+    opts.m3u8_uri_prefix=strdup(buf);
+    printf("inputip:%s:%d\n"
+           "outputip:%s:%d\n"
+           "m3u8filename:%s\n"
+           "m3u8prefix:%s\n"
+           "m3u8max:%d,durtion:%d\n"
+           "m3u8_uri_prefix:%s\n",opts.input_ip,opts.input_port,opts.output_ip,opts.output_port,
+           opts.m3u8_filename,opts.m3u8_prefix,opts.m3u8_maxnum,opts.m3u8_duration,
+           opts.m3u8_uri_prefix);
+
+    #endif
+    #if 1
+    g_HttpServer_Sem = malloc(sizeof(sem_t));
+    sem_init(g_HttpServer_Sem, 0 ,0);
+
+    buffer_queue=iSTB_Buffer_create(BF_RINGBUF_SIZE); 
+    istb_udp_event_subscribe(Udp_Event_Callback);
+    //udp_tid   =istb_udp_recv_start("127.0.0.1",1234,udp_write_data);
+    udp_tid   =istb_udp_recv_start(opts.input_ip,opts.input_port,udp_write_data);
+    istb_m3u8_event_subscribe(M3u8_Event_Callback);
+    memset(buf,0,sizeof(buf));
+    snprintf(buf,sizeof(buf),"-n %d -d %d -p %s -m %s -u %s",opts.m3u8_maxnum,opts.m3u8_duration,opts.m3u8_prefix,
+             opts.m3u8_filename,opts.m3u8_uri_prefix);
+    //m3u8_tid=istb_m3u8_start("-n 15 -d 10 -p tmp/istb -m tmp/yuxiang.m3u8 -u http://127.0.0.1:8080/",m3u8_read_data);
+    m3u8_tid=istb_m3u8_start(buf,m3u8_read_data);
+     sem_wait(g_HttpServer_Sem);
+     mg_io_fun *http_io_opreate=calloc(1,sizeof(mg_io_fun));
+     http_io_opreate->mg_io_open=istb_m3u8_api_open;
+     http_io_opreate->mg_io_read=istb_m3u8_api_read;
+     http_io_opreate->mg_io_write=istb_m3u8_api_write;
+     http_io_opreate->mg_io_seek=istb_m3u8_api_seek;
+     http_io_opreate->mg_io_close=istb_m3u8_api_close;
+     //httpserver_tid=istb_httpserver_create(8080,".",http_io_opreate);
+    memset(buf,0,sizeof(buf));
+    snprintf(buf,sizeof(buf),"%s:%d",opts.output_ip,opts.output_port);
+    httpserver_tid=istb_httpserver_create(buf,".",http_io_opreate);
+#if 0
+    printf("%s:sleep 30s \n",__FUNCTION__);
+    usleep(1000*1000*20);//5 min
+    printf("%s:now destory \n",__FUNCTION__);
+    myterminate=1;
+#endif
+     printf("%s now waiting...\n",__FUNCTION__);
+     act.sa_handler = &main_handler;
+
+     sigaction(SIGINT, &act, NULL);
+     sigaction(SIGTERM, &act, NULL);
+     while (!myterminate) {
+         usleep(1000*1000);
+     }
+    //destory the thread process ,anf free memory buffer
+    istb_m3u8_stop(m3u8_tid); 
+    istb_udp_recv_destroy(udp_tid);
+    istb_httpserver_destroy(httpserver_tid);
+    iSTB_Buffer_destroy(buffer_queue);
+    free(opts.input_ip);opts.input_ip=NULL;
+    free(opts.output_ip);opts.output_ip=NULL;
+    free(opts.m3u8_filename);opts.m3u8_filename=NULL;
+    free(opts.m3u8_prefix);opts.m3u8_prefix=NULL;
+    free(opts.m3u8_uri_prefix);opts.m3u8_uri_prefix=NULL;
+    #endif
+    return 0;
+}
 
